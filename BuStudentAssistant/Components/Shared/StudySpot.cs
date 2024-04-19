8888a4f1257af800d64d822b125b03c71f25a3dd
Dia Villalpando <die.villalpando@gmail.com>
2024-04-19 12:34:13 -0400

    moved CS code to new files, created subpage for each individual study space, and cleaned up the StudySpotYelp code

---
+++ BuStudentAssistant/Components/Shared/StudySpot.cs
@@ -0,0 +1,11 @@
+﻿namespace StudySpotYelpNameSpace;
+
+internal class StudySpot
+{
+    public int Id { get; set; }
+    public float Score { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public int Map_Marker { get; set; }
+    
+}
