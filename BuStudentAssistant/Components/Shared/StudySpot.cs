0b31e9bfd3f8d44ed5578810720a6cba315755e1
Dia Villalpando <die.villalpando@gmail.com>
2024-04-21 16:13:26 -0400

    added basic layout for the comments

--- BuStudentAssistant/Components/Shared/StudySpot.cs
+++ BuStudentAssistant/Components/Shared/StudySpot.cs
@@ -8,5 +8,14 @@ internal class StudySpot
     public string? Address { get; set; }
     public int Map_Marker { get; set; }
     public string? Description { get; set;}
+    public List<Comment>? Comments { get; set; }
     
+}
+
+internal class Comment{
+    public int Id { get; set; }
+    public DateOnly Date { get; set; }
+    public string? Text { get; set; }
+    public int User_Id { get; set; }
+    public string? User_Name { get; set; }
 }
