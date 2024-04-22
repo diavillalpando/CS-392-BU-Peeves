5dec5dd8a660b6137ddffd5152f05b839ae353e3
Dia Villalpando <die.villalpando@gmail.com>
2024-04-21 20:09:10 -0400

    successful connection to mongodb

---
+++ BuStudentAssistant/Components/Data/MongoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BuStudentAssistant.Models;
+using MongoDB.Driver;
+
+namespace BuStudentAssistant.Data
+{
+    public class MongoDbContext : IMongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("MongoDB"));
+            _database = client.GetDatabase("LoginDb");
+        }
+
+        public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+    }
+}
