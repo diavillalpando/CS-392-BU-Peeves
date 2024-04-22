0261557f1a8e947d1ff60333e48f35426fbe248f
anadt@bu.edu <anadt@bu.edu>
2024-04-22 13:13:29 -0400

    OAuth progress

--- BuStudentAssistant/Program.cs
+++ BuStudentAssistant/Program.cs
@@ -4,6 +4,7 @@ using BuStudentAssistant.Services;
 using Majorsoft.Blazor.Components.Maps;
 using Majorsoft.Blazor.Components.Common.JsInterop;
 using BuStudentAssistant.Data;
+using BuStudentAssistant.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,7 @@ builder.Services.AddScoped<UserService>();
 builder.Services.AddMapExtensions();
 builder.Services.AddJsInteropExtensions();
 builder.Services.AddSingleton<IMongoDbContext, MongoDbContext>();
+builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 
@@ -27,6 +29,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
